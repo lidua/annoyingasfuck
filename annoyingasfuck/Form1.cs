d70deb9b947ae07e62c223e113a033480308809d
lidua <63567002+lidua@users.noreply.github.com>
2020-04-12 20:30:12 -0400

    add files

---
+++ annoyingasfuck/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace annoyingasfuck
+{
+    public partial class Form1 : Form
+    {
+        public static int score;
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "score = " + score;
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.Location = new Point(button1.Location.X, button1.Location.Y + 50);
+            this.Height += 50;
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            button1.Location = new Point(button1.Location.X, button1.Location.Y + 50);
+            this.Height += 50;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.Location = new Point(button1.Location.X, button1.Location.Y + 50);
+            this.Height += 50;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            score += 1;
+            label1.Text = "score = " + score;
+        }
+    }
+}
